3632b57e4fe2e1a20d3b74ccb2ebfcdf00956085
Gregory Pacheco <gpzim98@gmail.com>
2018-11-11 19:39:36 +0000

    aula 9 renderizando pagina produto

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import HomeView
+from .views import (
+    HomeView,
+    CategoriaView,
+    ProdutoView,
+    CarrinhoView
+)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='core_home'),
+    path('categoria', CategoriaView.as_view(), name='core_categoria'),
+    path('produto', ProdutoView.as_view(), name='core_produto'),
+    path('carrinho', CarrinhoView.as_view(), name='core_carrinho'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -2,4 +2,16 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
+    template_name = "wish/index.html"
+
+
+class CategoriaView(TemplateView):
     template_name = "wish/categories.html"
+
+
+class ProdutoView(TemplateView):
+    template_name = "wish/product.html"
+
+
+class CarrinhoView(TemplateView):
+    template_name = "wish/cart.html"
