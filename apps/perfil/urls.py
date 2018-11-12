d6bd40aa9cd5e9374dab63a658359bb90c201f42
Gregory Pacheco <gpzim98@gmail.com>
2018-11-12 22:40:31 +0000

    aula 14 - Criando Pagina Meus Pedidos

--- apps/perfil/urls.py
+++ apps/perfil/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     PerfilView,
+    MeusPedidos
 )
 
 urlpatterns = [
     path('', PerfilView.as_view(), name='perfil_home'),
+    path('meus-pedidos/', MeusPedidos.as_view(), name='perfil_meus_pedidos'),
 
 ]
--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class PerfilView(TemplateView):
     template_name = "wish/perfil.html"
+
+
+class MeusPedidos(TemplateView):
+    template_name = "wish/meus_pedidos.html"
